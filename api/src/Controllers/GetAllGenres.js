a2d4e4a20fbb4ea622f6f73f5906f27ae0c74d5f
Franco Rocamora <martin.rocamoraf@gmail.com>
2023-04-27 13:24:29 -0300

    bug fix in database

--- api/src/Controllers/GetAllGenres.js
+++ api/src/Controllers/GetAllGenres.js
@@ -4,15 +4,22 @@ const {API_KEY} = process.env
 const {Genre} = require('../db')
 
 const getAllGenres = async () => {
-    const genresArray = []
-    const addToBdd = []
-    const response  = await axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`)
-    response.data.results.forEach(element => {
-        addToBdd.push({name: element.name})
-        genresArray.push(element.name)
-    });
-    const responsebdd = await Genre.bulkCreate(addToBdd)
-    return genresArray
+    const getAllGenres = async () => {
+        const genresArray = []
+        const addToBdd = []
+        const data = await Genre.findAll()
+        const response  = await axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`)
+        response.data.results.forEach(element => {
+            addToBdd.push({name: element.name})
+            genresArray.push(element.name)
+        });
+        if(!data.length){
+        const responsebdd = await Genre.bulkCreate(addToBdd)}
+        else{
+            return genresArray
+        }
+        return genresArray
+    }
 }
 
 
